7c2b866cb71200fbb1d2ef3d60bbf648bb039d22
Juan Jose <juanjoimbol@gmail.com>
2024-10-10 16:16:52 -0500

    Se agregan cambios al proyecto de hamburguesas

---
+++ selectivasProject/Main.py
@@ -0,0 +1,33 @@
+# Simple
+a = 33
+b = 200
+
+if b > a:
+    print(b,"es mayor que", a)
+
+#Doble
+y = 200
+z = 333
+
+if y > z:
+    print(y," es mayor que ", z)
+
+else:
+    print(y," no es mayor que ", z)
+
+#Multiple
+a = 200
+b = 207
+if a > b:
+    print(a," es mayor que ",b)
+elif a < b:
+    print(a," es menor que ", b)
+
+x = 28
+
+if x > 10:
+    print("por encima de diez, ")
+    if x > 20:
+        print("y tambien por encima de 20!")
+    else:
+        print("Pero no por encima de 20.")
